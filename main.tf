2b8e65ea9b5022f60b16da21b153a28ebc74553b
Photon92 <92054692+Photon92@users.noreply.github.com>
2022-11-08 09:06:58 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
 # Author: Persell Machuca
-# Blog: https://www.virtjunkie.com/vmware-provisioning-using-hashicorp-terraform-part-2/
-# GitHub: https://github.com/jonhowe/Terraform-vSphere-VirtualMachine/blob/master/main.tf
 # Vcenter connection parameters
 provider "vsphere" {
   user                 = var.vsphere_user
